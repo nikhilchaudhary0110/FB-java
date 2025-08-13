41c4cea4cfa61400bea7a18e5375a20dcc7a275d
Nikhil Chaudhary 1027 <nikhil.chaudhary_cs24@gla.ac.in>
2025-08-13 09:24:05 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main{
+    public static void main (String[] args) {
+        System.out.println("Your name");
+        System.out.println("Roll.no");
+        System.out.println("Class");
+    }
+}
