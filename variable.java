2fdac881c165acffb4fcb8fff33c9a0651c1d9fd
Nikhil Chaudhary 1027 <nikhil.chaudhary_cs24@gla.ac.in>
2025-08-13 10:02:57 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    static int y=2;//static variable
+    public static void main (String[] args){
+        int x = 10;//local variable
+        System.out.println(x);
+        System.out.println(variable.y);
+    }
+}
