bedbed3d0925c37e1cd783602fb1ebcd4104733c
Nikhil Chaudhary 1027 <nikhil.chaudhary_cs24@gla.ac.in>
2025-08-13 14:49:19 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,9 @@
+public class Assignment{
+    public static void main(String[] args) {
+        int a=6;
+        System.out.println(a);
+        System.out.println(a+=2);
+        System.out.println(a-=2);
+        System.out.println(a*=2);
+        System.out.println(a/=2);   
+}}
