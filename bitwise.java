3a134e73a869109d6c3de3710e7c505a328609ed
Nikhil Chaudhary 1027 <nikhil.chaudhary_cs24@gla.ac.in>
2025-08-14 10:25:46 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,8 @@
+public class bitwise{
+    public static void main(String[] args) {
+        int a= 6,b=5;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(a<<b);
+        System.out.println(a>>b);}}
