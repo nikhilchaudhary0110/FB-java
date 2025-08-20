32964b2e8d6914acf6fcecc3aabb1a5d104426d7
Nikhil Chaudhary 1027 <nikhil.chaudhary_cs24@gla.ac.in>
2025-08-20 09:20:57 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Switch {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the case:");
+        int day=sc.nextInt();
+        switch (day){
+            case 1:System.out.println("monday");break;
+            case 2:System.out.println("tuesday");break;
+            case 3:System.out.println("wednesday");break;
+            case 4:System.out.println("thursday");break;
+            case 5:System.out.println("friday");break;
+            case 6:System.out.println("satday");break;
+            case 7:System.out.println("sunday");break;
+            default:System.out.println("Invalid day");
+
+        }
+    }
+    
+}
---
+++ even.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class even {
+    public static void main(String[] args){
+    Scanner even=new Scanner(System.in);
+    System.out.println("enter a value:");
+    int n=even.nextInt();
+    if(n%2==0){
+        System.out.println("even number");
+    }
+    else{
+        System.out.println("odd number");
+    }}
+    
+}
