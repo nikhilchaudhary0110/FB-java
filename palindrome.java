04ddb56dd8231435ea0cde34875bcd24e1b38b25
Nikhil Chaudhary 1027 <nikhil.chaudhary_cs24@gla.ac.in>
2025-08-20 08:54:50 +0530

    check whether number is palindrome or not

---
+++ palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num=sc.nextInt();
+        int originalnum=num;
+        int reversenum=0;
+        while(num!=0){
+            int digit=num%10;
+            reversenum=reversenum*10+digit;
+            num/=10;
+        }
+        if(originalnum==reversenum){
+            System.out.println(originalnum+" is a palindrome");
+        }
+        else{
+            System.out.println(originalnum+" is not a palindrome");
+        }
+        sc.close();
+    }
+    
+}
